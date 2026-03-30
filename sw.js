48b02d6acfddc4935a75cf791c91b6d54774cc37
ValerioLam <lampisvalerio@gmail.com>
2026-03-30 16:15:15 +0200

    Add files via upload
    Aggiunta musica e foto

--- sw.js
+++ sw.js
@@ -1,22 +1,16 @@
-const cacheName = 'relax-game-v1';
+const cacheName = 'relax-game-v2';
 const assets = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './faccia.png',
+  './musica.mp3'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
