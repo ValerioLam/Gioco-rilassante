b187bc93de93ff1995488799974a76fccb84aa1b
ValerioLam <lampisvalerio@gmail.com>
2026-03-30 16:46:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const cacheName = 'relax-game-v2';
+// Aggiorniamo la versione qui da v2 a v3
+const cacheName = 'relax-game-v3'; 
 const assets = [
   './',
   './index.html',
@@ -8,9 +9,32 @@ const assets = [
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      // Forza l'attivazione immediata del nuovo SW
+      self.skipWaiting(); 
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  // Cancella la vecchia cache v2
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
